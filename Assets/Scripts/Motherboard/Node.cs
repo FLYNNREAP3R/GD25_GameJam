6a1ea235c18836632cceb4a4d8091b5493407c56
FLYNNREAP3R <111404122+FLYNNREAP3R@users.noreply.github.com>
2025-05-22 21:09:21 -0400

    turret placement

---
+++ Assets/Scripts/Motherboard/Node.cs
@@ -0,0 +1,36 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Node : MonoBehaviour
+{
+    private Color startColor;
+    public Color hoverColor;
+
+    private Renderer rend;
+    private TilemapCollider2D tileColl;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        tileColl = GetComponent<TilemapCollider2D>();
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Please work");
+    }
+
+    void OnCollisionEnter2D()
+    {
+       Debug.Log("Come on man");
+       rend.material.color = hoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
